91c1eddc3b2f99fc88caeef9681e3ba1ebc5f33a
Julien Dhouti <julien.dhouti@gmail.com>
2018-01-26 21:58:20 -0700

    all portfolio functions don't check for entry anymore

--- portfolio.py
+++ portfolio.py
@@ -1,7 +1,8 @@
 # Manages all portfolio functions to keep bot.py cleaner
 
-import boto3
+import boto3, requests, json
 import database_lookup as database
+
 db = boto3.resource('dynamodb')
 pf = db.Table('portfolios')
 
@@ -10,51 +11,80 @@ def create(user_id, p_name):
     Will return True or False based on whether it was dable to do so."""
 
     ## Check if user already has a portfolio with the same name
-    if database.existance(user_id):
-        if database.portfolio_existance(user_id, p_name):
-            return False
+    if database.portfolio_existance(user_id, p_name):
+        return False
         
-        # Otherwise we just update the list of all portfolios for that user here.
-        else:
-            pf.update_item(
-                Key={
-                    'user': user_id
-                },
-                UpdateExpression=f"set portfolios.{p_name} = :r",
-                ExpressionAttributeValues={
-                    ':r':{'tickers':{}, 'value':0}
-                }
-            )
-    
-    # Since the 'Item' is not in the potential dictionary, user was not created so do that here.
+    # Otherwise we just update the list of all portfolios for that user here.
     else:
-        pf.put_item(
-            Item={
-                'user': user_id,
-                'portfolios': {
-                    p_name: {
-                        'tickers': {},
-                        'value': 0
-                    }
-                }
+        pf.update_item(
+            Key={
+                'user': user_id
+            },
+            UpdateExpression=f"set portfolios.{p_name} = :r",
+            ExpressionAttributeValues={
+                ':r':{'tickers':{}, 'value':0}
             }
         )
-        
+
     return True
+
+
+def delete(user_id, p_name):
+    # Check to see if the given database is actually inside the DB
+    if not database.portfolio_existance(user_id, p_name):
+        return False
+    
+    # If not, begin deletion...
+    else:
+        portfolios = view_all(user_id)
+        del portfolios[p_name]
+
+        pf.update_item(
+            Key={
+                'user': user_id
+            },
+            UpdateExpression=f"set portfolios = :r",
+            ExpressionAttributeValues={
+                ':r': portfolios
+            }
+        )
+
+    return True
+
+
+def update_values(user_id, p_name):
+    """Will update the value of the portfolio."""
+
+    portfolio_tickers = view(user_id)['tickers']
+    total_value, ticker_strings = 0, ''
+
+    for i in portfolio_tickers.keys():
+        ticker_strings += i + ','
+
+    raw_data = requests.get(f'https://api.iextrading.com/1.0/stock/market/batch?symbols={ticker_strings}&types=quote')
+    data = json.loads(raw_data.text)
+
+    prices =[]
+
+    for i in data:
+        prices.append(i['quote']['latestPrice'])
+
+    for price, amount in zip(prices, ticker_strings.values()):
+        total_value += price * amount
+
+    # update the dictionary
+
 
 def view_all(user_id):
     """Returns all portfolio names in a list for the given user."""
-    if not database.existance(user_id):
-        return None
 
-    else:
-        names = pf.get_item(
-            Key={
-                'user': user_id
-            }
-        )['Item']['portfolios'].keys()
+    portfolios = pf.get_item(
+        Key={
+            'user': user_id
+        }
+    )['Item']['portfolios']
 
-        return list(names)
+    return portfolios
 
 def view(user_id, p_name):
     """Returns the information of a specific portfolio. Returns a dictionary."""
